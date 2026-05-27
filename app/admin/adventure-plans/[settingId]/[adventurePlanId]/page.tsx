1f6bea2a29c5f8b00387e0d962b2c1c4e2882063
John Polacek <johnpolacek@gmail.com>
2026-05-27 10:17:22 -0500

    Redirect admin adventure plan detail routes

---
+++ app/admin/adventure-plans/[settingId]/[adventurePlanId]/page.tsx
@@ -0,0 +1,14 @@
+import { redirect } from "next/navigation"
+
+type AdminAdventurePlanRedirectPageProps = {
+  params: Promise<{
+    settingId: string
+    adventurePlanId: string
+  }>
+}
+
+export default async function AdminAdventurePlanRedirectPage({ params }: AdminAdventurePlanRedirectPageProps) {
+  const { settingId, adventurePlanId } = await params
+
+  redirect(`/settings/${settingId}/${adventurePlanId}/edit`)
+}
